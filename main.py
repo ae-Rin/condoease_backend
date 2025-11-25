75d118133f3bc96fa586e089d85e473c0a3f237c
Rin <banlosairen@gmail.com>
2025-11-25 16:54:00 +0800

    revert get api for props

--- main.py
+++ main.py
@@ -507,15 +507,7 @@ def get_all_property_owners(token: dict = Depends(verify_token)):
 def get_all_properties(token: dict = Depends(verify_token)):
     db = get_db()
     cursor = db.cursor(as_dict=True)
-    # cursor.execute("SELECT * FROM properties")
-    cursor.execute("""
-        SELECT 
-            p.*,
-            po.first_name + ' ' + po.last_name AS owner_full_name
-        FROM properties p
-        LEFT JOIN property_owners po ON p.registered_owner = po.owner_id
-        ORDER BY p.created_at DESC
-    """)
+    cursor.execute("SELECT * FROM properties")
     return cursor.fetchall()
 
 @app.get("/api/property-units")
